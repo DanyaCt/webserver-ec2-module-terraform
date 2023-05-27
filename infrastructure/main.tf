4f7db7273e5cd2462344890366d1eb96374c7eda
DanyaCt <sebtec1212@gmail.com>
2023-05-27 16:53:50 +0300

    Add state to S3

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {  
+    bucket = "cicd-its-task"    
+    key = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
